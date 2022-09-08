ca38ca3b8309ae70d4b95ff3357f33d47ef5da66
jiawayp <pauljiaway4@gmail.com>
2022-09-08 12:19:45 -0700

    Hello-World Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * Main-Entry: Entry point for all programs
+ *
+ * Description: Print the size of data types
+ *
+ * Return: Always return 0 (sucess)
+ */
+int main(void)
+{
+	char;
+	int;
+	long int;
+	long long;
+	float;
+	printf("Size of char: %d-byte(s)\n", sizeof(char));
+	printf("Size of an int: %d-byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d-byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %d-byte(s)\n", sizeof(long long));
+	printf("Size of a float: %d-byte(s)\n", sizeof(float));
+}
