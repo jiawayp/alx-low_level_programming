05f4229dac410569161eac18ba612b9394f4c439
jiawayp <pauljiaway4@gmail.com>
2022-09-08 12:32:24 -0700

    Hello-World Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,6 @@
  */
 int main(void)
 {
-	char;
-	int;
-	long int;
-	long long;
-	float;
 	printf("Size of char: %d byte(s)\n", (int) sizeof(char));
 	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
