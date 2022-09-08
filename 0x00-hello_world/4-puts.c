a37e4a7df963de6d074970261d38d772a688519b
jiawayp <pauljiaway4@gmail.com>
2022-09-08 11:28:55 -0700

    Hello World Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 /**
+ * The main function which is the entry point for runing programs
+ *
  * This will print a message to the screen
+ *
+ * Return value of zero
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
