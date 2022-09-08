844c3c8b0061cac9f499ff408e945b04f51ff40d
jiawayp <pauljiaway4@gmail.com>
2022-09-08 11:51:19 -0700

    Hello World Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main-entry: Main Function of the program
+ *
+ * Description: This will print a message to the screen in the printf function
+ *
+ * Return: Always return 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+	return (0);
+}
