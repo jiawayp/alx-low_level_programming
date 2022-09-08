8e6001985fc3351b39ebae381b85f4e7923c351b
jiawayp <pauljiaway4@gmail.com>
2022-09-08 11:18:53 -0700

    Hello World Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
-int main()
+/**
+ * This will print a message to the screen
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
