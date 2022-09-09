a62ac9a0720ee66374b3e864f7d557ee1fd63a91
jiawayp <pauljiaway4@gmail.com>
2022-09-09 16:53:38 -0700

    Task 8 Hexadecimal number

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main-entry for running the program
+ *
+ * Description: ouput Hexadecimal numbers
+ *
+ * Return: return 0 (sucess)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 47)
+	{
+		if (x < 10)
+		{
+			putchar(x + '0');
+		}
+		else if (x > 41)
+		{
+			putchar(x - 10 + 'A');
+		}
+		x++
+	}
+	putchar(10);
+
+	return (0);
+}
