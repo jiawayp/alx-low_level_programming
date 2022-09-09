edae7818cefe1f9f5102f68590a43d8d399c3436
jiawayp <pauljiaway4@gmail.com>
2022-09-09 16:24:17 -0700

    Task 7 Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *
+ * main entry for the program
+ *
+ * Description: output alphabet in reverse
+ *
+ * Return: return 0 (sucess)
+ */
+int main(void)
+{
+	int r_alph = 122;
+
+	while (r_alph >= 97)
+	{
+		putchar(r_alph);
+		r_alph--;
+	}
+	putchar(10);
+
+	return (0);
+}
