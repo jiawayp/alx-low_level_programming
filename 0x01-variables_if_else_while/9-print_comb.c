47c32823bfd2590cee747c77d59b79214e808039
jiawayp <pauljiaway4@gmail.com>
2022-09-10 13:40:07 -0700

    Task 9 Print number separated by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main-entry: entry point that run every program
+ *
+ * Description: output number separated by commas.
+ *
+ * Return: return 0 (sucess)
+ */
+int main(void)
+{
+	int x = 48;
+	
+	while (x <= 57)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		x++;
+	}
+	putchar(10);
+
+	return (0);
+
+}
