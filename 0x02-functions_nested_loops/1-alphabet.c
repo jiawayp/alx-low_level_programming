00c369cad8a06632cb6c53986d7c35e7e92d7224
jiawayp <pauljiaway4@gmail.com>
2022-09-13 15:05:29 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main entry of program;
+ *
+ * Descriptions: out letter in lowercase
+ *
+ * Return: return 0 (sucess)
+ */
+void print_alphabet(void)
+{
+	char lower_alph = "z";
+	
+	for (lower_alph = "a"; lower_alph == "z"; lower_alph++)
+	{
+		_putchar(lower_alph);
+	}
+	_putchar("\n");
+}
