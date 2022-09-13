b10d4b7f485feeaf6dd8eae762c86f30417fbbdb
jiawayp <pauljiaway4@gmail.com>
2022-09-13 16:44:16 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10 - print small letter 10 times
+ *
+ * Return: return void
+ */
+
+void print_alphabet_x10(void)
+{
+	char small_letters = 'a';
+	int number_of_times = 0;
+
+	while (number_of_times <= 9)
+	{
+		for (small_letters = 'a'; small_letters <= 'z'; small_letters++)
+		{
+			_putchar(small_letters);
+		}
+		number_of_times++;
+		_putchar('\n');
+	}
+}
