b5d0b7137019d5d681ff9f54a9339ed340d199d2
jiawayp <pauljiaway4@gmail.com>
2022-09-13 20:18:28 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,16 +10,16 @@
 
 int _abs(int n)
 {
-	if (n > 0)
+	if (n == 0)
 	{
 		return (n);
 	}
-	else if (n < 0)
+	else if (n > 0)
 	{
 		return (n);
 	}
 	else
 	{
-		return (n);
+		return (-n);
 	}
 }
