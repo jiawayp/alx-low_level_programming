d185861ab9288556024f42f8df21be5afa9459f7
jiawayp <pauljiaway4@gmail.com>
2022-09-13 17:33:12 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * islower - print 1 for lower and 0 for upper
+ *
+ * Return: return 1 if c is lower
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' & c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
