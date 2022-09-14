acfada899e533bc841d3f4247df8478158111788
jiawayp <pauljiaway4@gmail.com>
2022-09-13 19:53:50 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,16 +3,16 @@
 /**
  * print_sign - of numbers
  *
- * Descriptions - print +, - or 0 
+ * @n - print +, - or 0 
  *
- * Return -1, 0, or -1
+ * Return: return -1, 0, or -1
  */
 
 int print_sign(int n)
 {
-	if ( n > 0)
+	if (n > 0)
 	{
-		_putchar('+');
+		_putchar('1');
 		return (1);
 	}
 	else if (n < 0)
