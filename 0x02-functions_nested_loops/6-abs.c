daa030eaacb0aa2e411101ff751a8c49c8c810f0
jiawayp <pauljiaway4@gmail.com>
2022-09-13 20:11:54 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the absolute value
+ *
+ * @n: is an integer
+ *
+ * Return: return int
+ */
+
+int _abs(int n)
+{
+	if (n == 0)
+	{
+		return (-);
+	}
+	else if (n > 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
