4e688911ab037a25f2ad912ad89697e16fba88ee
jiawayp <pauljiaway4@gmail.com>
2022-09-14 17:00:18 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function prototype
+ *
+ * @n1: is an integer
+ * @n2: is also an integer
+ *
+ * Description: add two number
+ *
+ * Return: the sum 
+ */
+
+int add(int n1, int n2)
+
+	int sum = n1 + n2;
+
+	return (sum);
