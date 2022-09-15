e591603351beceaeec36ee93299db3f851b8f2a5
jiawayp <pauljiaway4@gmail.com>
2022-09-15 06:21:17 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * isupper - prototype function
+ * @c: check for upper
+ *
+ * Return: return 1 if c is upper, and 0 if not
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') & (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This is a more file on function.
+Task 0 print 1 if letter is uppercase otherwise it return 0
