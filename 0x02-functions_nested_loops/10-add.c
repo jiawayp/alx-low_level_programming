3dc23c69066f3e4a463fad9f48176e75d40fd2d2
jiawayp <pauljiaway4@gmail.com>
2022-09-14 17:18:11 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -10,11 +10,12 @@
  *
  * Return: the sum 
  */
-int add(int num1, int num2)
-{
-	int sum;
-	
-	sum = num1 + num2;
 
-	return (sum);
+int add(int num1,int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+	return (result);
 }
