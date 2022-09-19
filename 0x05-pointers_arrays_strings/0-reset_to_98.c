93e51cc34e7ba1a3c5610271401b1974d04a47bd
jiawayp <pauljiaway4@gmail.com>
2022-09-19 10:15:34 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function of the programm
+ * @n: pointer
+ * Return: void (nothing)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Introduction to pointers and array in c
+1- Reset_to_98
