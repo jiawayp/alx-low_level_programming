2926e1fdc2e2dfee07c1db0283ab461cc1340ddf
jiawayp <pauljiaway4@gmail.com>
2022-09-20 10:30:44 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,19 +10,22 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
-	int count = 1;
+	int i, count;
 
+	i = 0;
+	count = 1;
 	if (n < 0)
-		n =0;
+	{
+		n = 0;
+	}
 	if (n > 0)
 	{
-		do 
+		while (a[i] != '\0' && count < n)
 		{
 			printf("%d, ", a[i]);
 			i++;
 			count++;
-		} while (a[i] != '\0' && count < n);
+		}
 		printf("%d\n", a[i]);
 	}
 }
