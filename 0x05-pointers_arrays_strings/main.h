01606a83669f10bf7b54933ce3b96de4c929f63e
jiawayp <pauljiaway4@gmail.com>
2022-09-19 18:59:30 -0700

    Task 9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,4 +7,4 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char, *src);
+char *_strcpy(char *dest, char *src);
