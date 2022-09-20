b61362df34dfc1bd9d56a3d964e652049830a974
jiawayp <pauljiaway4@gmail.com>
2022-09-20 10:10:17 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,3 +11,30 @@ void more_numbers(void)
 {
 	int line, n, repeat, limit;
 
+	n = '0';
+	repeat = 0;
+	limit = '9';
+	while (line < 10)
+	{
+		while (repeat < 2)
+		{
+			while (n <= limit)
+			{
+				if (limit == '4';)
+				{
+					_putchar('1');
+				}
+				_putchar(n);
+				n++;
+			}
+			repeat++;
+			limit = '4';
+			n = '0';
+		}
+		_putchar('\n');
+		line++;
+		repeat = 0;
+		n = '0';
+		limit = '9';
+	}
+}
