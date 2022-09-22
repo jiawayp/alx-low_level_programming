0d5cc82cf1fae5382c29a5c526dcf3053585dad8
jiawayp <pauljiaway4@gmail.com>
2022-09-22 16:57:33 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - character string to uppercase 
+ * @s: string array
+ * Return: s
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
