1a9f912f902f0d427dc7f94cb24748111dadd05c
jiawayp <pauljiaway4@gmail.com>
2022-09-27 07:33:04 -0700

    Advance Task 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set value of pointer to char
+ * @s: pointer
+ * @to: char pointer
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
