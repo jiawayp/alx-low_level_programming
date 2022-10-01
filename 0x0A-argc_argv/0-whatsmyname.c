d0aeb6c9f3aa800717efc99dca48ba5406a564e9
jiawayp <pauljiaway4@gmail.com>
2022-10-01 08:23:11 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of arg
+ * @argv: content of arg
+ *Return: 0 success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+Low level programming with alx 
+argc, argv tasks
