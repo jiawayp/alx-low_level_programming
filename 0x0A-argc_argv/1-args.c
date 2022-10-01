8735c30a82e0ee441b6d67166eec05be291aa567
jiawayp <pauljiaway4@gmail.com>
2022-10-01 08:40:07 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 sucess
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (argv);
+}
