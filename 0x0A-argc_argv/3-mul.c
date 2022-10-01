4b3c423372868e256dbab3b60c2c3feeda2ffe23
jiawayp <pauljiaway4@gmail.com>
2022-10-01 09:25:37 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: error if arg != 2 two
+ * return 0 if = 2 arg
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int prod = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		prod = prod * atoi(argv[i]);
+	}
+	printf("%d\n", prod);
+	return (0);
+}
