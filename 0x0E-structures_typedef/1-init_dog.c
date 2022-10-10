f33ee5736dc171348580066e9892c745fc290dd0
jiawayp <pauljiaway4@gmail.com>
2022-10-10 13:48:57 -0700

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,14 +10,14 @@
  * Return: nothing
  */
 
-void init_dog(struct dog *d, char *name, float age, char owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		;
 	else
 	{
-		d.name = name;
-		d.age = age;
-		d.owner = owner;
+		d -> name = name;
+		d -> age = age;
+		d -> owner = owner;
 	}
 }
