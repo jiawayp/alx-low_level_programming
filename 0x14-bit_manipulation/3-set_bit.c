e0f6b0942ffe3150ad3d3eef4eb548f67fa272dd
jiawayp <pauljiaway4@gmail.com>
2022-10-27 18:25:52 -0700

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,12 +9,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+
+	if (index >= 65)
+		return (-1);
 	if (!((*n >> index) & 1))
 	{
 		*n +=  1 << index;
 		return (1);
 	}
-	if (index >= 65)
-		return (-1);
 	return (-1);
 }
